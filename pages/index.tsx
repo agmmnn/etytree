5b1aa954a015ab0dae2fa68540e2ce2d7155f549
Gökçe <agmmnn@gmail.com>
2022-05-30 23:48:05 +0300

    border

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,21 +8,26 @@ export default function HomePage() {
     <>
       <ColorSchemeToggle />
       <Welcome />
-      <br />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
-          backgroundColor: '#0d0e17',
-          backgroundImage:
-            '-webkit-repeating-radial-gradient(top center, rgba(255,255,255,.1), rgba(255,255,255,.1) 1px, transparent 0, transparent 100%)',
-          backgroundSize: '20px 20px',
-        }}
-      >
-        <DataEty />
+      <div style={{ boxSizing: 'border-box', margin: 'auto', maxHeight: '100%' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            backgroundColor: '#0d0e17',
+            backgroundImage:
+              '-webkit-repeating-radial-gradient(top center, rgba(255,255,255,.1), rgba(255,255,255,.1) 1px, transparent 0, transparent 100%)',
+            backgroundSize: '20px 20px',
+            borderTop: '4px solid #17649d',
+            marginTop: '10px',
+            padding: '10px',
+            position: 'absolute',
+          }}
+        >
+          <DataEty />
+        </div>
       </div>
     </>
   );
